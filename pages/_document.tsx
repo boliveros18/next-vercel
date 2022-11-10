5dc01c1f2b43f35f5ce3f4d6cbf2684518ccf671
Bresneth Oliveros <boliveros18@gmail.com>
2022-11-10 08:21:31 -0500

    2nd

---
+++ pages/_document.tsx
@@ -0,0 +1,26 @@
+import Document, {Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx)
+
+    return initialProps
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head/>
+        <title>Advanced</title>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
